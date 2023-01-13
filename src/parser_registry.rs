dc44c9899b70e442275b3e0b3f4482939377a111
Robin Herrmann <Robin.Herrmann@dormakaba-external.com>
2023-01-13 12:39:42 +0100

    add rust cache

--- src/parser_registry.rs
+++ src/parser_registry.rs
@@ -28,3 +28,62 @@ pub fn parse(payload: &str) -> RegistryType {
     }
     RegistryType::None
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_set_name() {
+        // given
+        let payload: &str = r#"{"SetName":{"id":1,"name":"new_thing"}}"#;
+
+        // when
+        let result: RegistryType = parse(payload);
+
+        // then
+        let expected = RegistryType::SetNameType(SetNameDescirption {
+            set_name: SetName {
+                id: 1,
+                name: "new_thing".into(),
+            },
+        });
+        // assert_eq!(expected, result);
+    }
+
+    // #[test]
+    // fn parse_fails_with_empty_string() {
+    //     // given
+    //     let payload: &str = "";
+
+    //     // when
+    //     let result = parse(payload);
+
+    //     // then
+    //     assert!(result.is_err());
+    // }
+
+    // #[test]
+    // fn parse_fails_with_invalid_json() {
+    //     // given
+    //     let payload: &str = r#"{"WrongMessage":{"id":1,"name":"new_thing"}}"#;
+
+    //     // when
+    //     let result = parse(payload);
+
+    //     // then
+    //     assert!(result.is_err());
+    // }
+
+    // #[test]
+    // fn create_success() {
+    //     // given
+    //     let expected: &str = r#"{"SetName":{"id":1,"name":"new_thing"}}"#;
+
+    //     // when
+    //     let result = create(1, "new_thing".into());
+
+    //     // then
+    //     assert_eq!(expected, result.as_str());
+    // }
+}
